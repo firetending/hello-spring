099ace657b6e580b97e25b1f9386aca2dccef8a7
firetending <alan.miller.375@gmail.com>
2022-09-15 22:39:49 -0400

    initial commit

---
+++ src/main/java/org/launchcode/hellospring/controllers/HelloController.java
@@ -0,0 +1,44 @@
+package org.launchcode.hellospring.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class HelloController {
+
+//    handles requests at path /hello
+    @GetMapping("hello")
+    @ResponseBody
+    public String Hello() {
+        return "Hello, Spring!";
+    }
+
+    @GetMapping("goodbye")
+    @ResponseBody
+    public String Goodbye() {
+        return "Goodbye, Spring!";
+    }
+
+//    // handler for requests of the for /hello?name=LaunchCode
+//    @GetMapping("hello")
+//    @ResponseBody
+//    public String helloWithQueryParam(@RequestParam String name) {
+//        return "Hello, " + name + "!";
+//    }
+//
+//    // handler for requests of the for /hello/LaunchCode
+//    @GetMapping("hello/{name}")
+//    @ResponseBody
+//    public String helloWithPathVariable(@PathVariable String name) {
+//        return "Hello, " + name + "!";
+//    }
+//
+//    // handler that redirects to a path rather than using @ResponseBody
+//    @GetMapping("helloRE")
+//    public String helloRedirect() {
+//        return "redirect:/hello/gotcha";
+//    }
+}
